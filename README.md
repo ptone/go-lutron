b35fd6bfedda8717b7d885b1958def093d129fac
Preston Holmes <ptone@google.com>
2016-12-18 08:38:26 -0800

    first step toward working with msgs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-lutron
 minimal golang library for interacting with telnet based Lutron integration protocol (for Caseta)
+
+http://www.lutron.com/TechnicalDocumentLibrary/040249.pdf
+
+
--- lutron.go
+++ lutron.go
@@ -3,25 +3,55 @@ package lutron
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"strings"
+	"time"
+)
+
+type MsgType int
+type Command string
+
+const (
+	Get MsgType = iota
+	Set
+	Watch
+	Response
+)
+
+const (
+	Output Command = "OUTPUT"
+	Device Command = "DEVICE"
+	Group  Command = "GROUP"
 )
 
 type Lutron struct {
 	hostName string
 	Port     string
 	conn     net.Conn
-	// TODO use teereader to split
-	// http://rodaine.com/2015/04/async-split-io-reader-in-golang/
-	// then have one split just read for \n - the other for >
-	// https://play.golang.org/p/P-7siRTjzA
-	reader    *bufio.Reader
-	Username  string
-	Password  string
+	reader   *bufio.Reader
+	Username string
+	Password string
+	// TODO make private and expect watch requests to get access to responses
 	Responses chan string
 	done      chan bool
-	// TODO device map
 	inventory Inventory
+}
+
+type LutronMsg struct {
+	// the lutron component number
+	Id    int
+	Name  string
+	Level float64
+	// duration in seconds for a set action
+	// TODO parse > 60 seconds into string "M:SS"
+	Fade float64
+	// the action to take with the command, Get, Set, Watch, Default: Get
+	Type MsgType
+	// the integration command type - Output, Device
+	Cmd Command
+	// TODO
+	// Action Number - default to 1 for now
 }
 
 // custom io scanner splitter
@@ -49,6 +79,28 @@ func NewLutron(hostName, inventoryPath string) *Lutron {
 		inventory: inv,
 	}
 	return l
+}
+
+// TODO make private when watch done
+// pulls from channel until it finds response
+// may just move to fully blocking channel fetch
+func (l *Lutron) GetResponse() (r string, err error) {
+	for {
+		select {
+		case r = <-l.Responses:
+			// ignore zero length blank responses
+			if len(r) > 0 {
+				fmt.Println("popped ", r)
+				// ignore GNET prompts
+				if string(r[:1]) == "~" {
+					return r, nil
+				}
+			}
+		default:
+			fmt.Println("no activity")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }
 
 func (l *Lutron) Connect() error {
@@ -81,7 +133,7 @@ func (l *Lutron) Connect() error {
 			select {
 			case <-l.done:
 				return
-			case l.Responses <- scanner.Text():
+			case l.Responses <- strings.TrimSpace(scanner.Text()):
 			}
 		}
 	}()
@@ -96,7 +148,7 @@ func (l *Lutron) Disconnect() error {
 // TODO - how many API variations to support - need to have one
 // with Fade
 func (l *Lutron) SetById(id int, level float64) error {
-	return l.Send(fmt.Sprintf("#OUTPUT,2,%d,%f", id, level))
+	return l.Send(fmt.Sprintf("#OUTPUT,%d,1,%f", id, level))
 }
 
 func (l *Lutron) SetByName(name string, level float64) error {
@@ -111,4 +163,37 @@ func (l *Lutron) SetByName(name string, level float64) error {
 func (l *Lutron) Send(msg string) error {
 	fmt.Fprintf(l.conn, msg+"\n")
 	return nil
+}
+
+func (l *Lutron) SendCommand(c *LutronMsg) (resp string, err error) {
+	var cmd string
+	if c.Id == 0 {
+		c.Id, err = l.inventory.IdFromName(c.Name)
+		if err != nil {
+			return "", err
+		}
+	}
+	if c.Cmd == "" {
+		c.Cmd = Output
+	}
+
+	switch c.Type {
+	case Get:
+		cmd = fmt.Sprintf("?%s,%d,1", c.Cmd, c.Id)
+		// TODO confirm level and fade are 0
+	case Set:
+		cmd = fmt.Sprintf("#%s,%d,1,%.2f", c.Cmd, c.Id, c.Level)
+	case Watch:
+		// TODO
+		// create mechanism to add a fmt.scanner on responses in a goroutine
+		// with a dedicated channel for matches
+		log.Fatal("Watch not implemented")
+	}
+
+	if c.Fade > 0.0 {
+		cmd = fmt.Sprintf("%s,%.2f", cmd, c.Fade)
+	}
+	fmt.Println("debug: ", cmd)
+	l.Send(cmd)
+	return l.GetResponse()
 }
